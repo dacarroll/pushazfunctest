883580b921f741afeb780dea368ec7cb1a1be2ae
Daniel Carroll <dacarr@microsoft.com>
2022-08-01 23:51:46 -0400

    trigger self-hosted

--- Functions/trigpunchhub/trigger/run.ps1
+++ Functions/trigpunchhub/trigger/run.ps1
@@ -2,7 +2,7 @@ using namespace System.Net
 
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
-
+#small change
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
