8fd610855fd062924a1a6bea7f77ff09b9cf1455
Daniel Carroll <dacarr@microsoft.com>
2022-09-24 19:29:39 -0400

    1 modify 1 delete

--- Functions/funcsapp/replytrig/run.ps1
+++ Functions/funcsapp/replytrig/run.ps1
@@ -7,15 +7,15 @@ param($Request, $TriggerMetadata)
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
+$names = $Request.Query.Name
+if (-not $names) {
+    $names = $Request.Body.Name
 }
 
 $body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
+if ($names) {
+    $body = "Hello, $names. This HTTP triggered function executed successfully."
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
